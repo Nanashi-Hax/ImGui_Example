4a1e8956dd37b8216882bd1d0330e370f6d498e4
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2025-07-16 12:43:34 +0900

    Editing main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,12 +6,16 @@
 
 #include <gx2/context.h>
 #include <gx2/enum.h>
+#include <whb/log_udp.h>
+#include <whb/log.h>
 
-WUPS_PLUGIN_NAME("imgui_overlay_plugin");
-WUPS_PLUGIN_DESCRIPTION("imgui_overlay_plugin");
+WUPS_PLUGIN_NAME("ImGui Example");
+WUPS_PLUGIN_DESCRIPTION("Overlay Plugin");
 WUPS_PLUGIN_VERSION("v1.0");
-WUPS_PLUGIN_AUTHOR("GaryOderNichts");
+WUPS_PLUGIN_AUTHOR("Nanashi-Hax");
 WUPS_PLUGIN_LICENSE("MIT");
+
+WUPS_USE_STORAGE("ImGui"); // Required for ON_APPLICATION_REQUESTS_EXIT to be called
 
 GX2ContextState* gContextState = nullptr;
 bool gOverlayInitDone = false;
@@ -26,16 +30,23 @@ INITIALIZE_PLUGIN()
 {
     gOverlayInitDone = false;
 
-    gContextState = (GX2ContextState *) MEMAllocFromMappedMemoryForGX2Ex(
-            sizeof(GX2ContextState),
-            GX2_CONTEXT_STATE_ALIGNMENT);
-    if (gContextState == nullptr) {
+    gContextState = (GX2ContextState *)MEMAllocFromMappedMemoryForGX2Ex
+    (
+        sizeof(GX2ContextState),
+        GX2_CONTEXT_STATE_ALIGNMENT
+    );
+
+    if (gContextState == nullptr)
+    {
         OSFatal("Failed to allocate gContextState");
     }
 }
 
 ON_APPLICATION_START()
 {
+    WHBLogUdpInit();
+    WHBLogPrintf("Initializing ImGui...");
+
     SetTV(new TV(ImVec2(1280.0f, 720.0f)));
     SetDRC(new DRC(ImVec2(854.0f, 480.0f)));
 }
@@ -44,6 +55,9 @@ ON_APPLICATION_REQUESTS_EXIT()
 {
     delete GetTV();
     delete GetDRC();
+
+    WHBLogPrintf("Shutting down ImGui...");
+    WHBLogUdpDeinit();
 }
 
 TV* GetTV()
