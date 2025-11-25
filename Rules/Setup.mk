f81af0e06344afbdeff2cfbe7206a78f598e162d
Nanashi-Hax <nanashi.rainbowhacker@gmail.com>
2025-11-25 22:12:59 +0900

    ファイル構成やMakefileを整理

---
+++ Rules/Setup.mk
@@ -0,0 +1,10 @@
+.SUFFIXES:
+
+ifeq ($(strip $(DEVKITPRO)),)
+$(error "Please set DEVKITPRO in your environment. export DEVKITPRO=<path to>/devkitpro")
+endif
+
+include $(DEVKITPRO)/wups/share/wups_rules
+
+WUT_ROOT := $(DEVKITPRO)/wut
+WUMS_ROOT := $(DEVKITPRO)/wums
--- wiiu/README.md
+++
@@ -1,2 +0,0 @@
-# ImGui backend
-Based on https://github.com/GaryOderNichts/imgui, adjusted to work with modules/plugins.
